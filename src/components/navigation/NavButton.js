5c8962de65a381f77af881bbcf753d1954514149
rajeev <rajeev.goonie98@gmail.com>
2020-03-29 14:24:21 -0700

    changed outer tag element

--- src/components/navigation/NavButton.js
+++ src/components/navigation/NavButton.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import Link from 'react-router-dom';
+import React, { Component } from "react";
+import Link from "react-router-dom";
 
 export class NavButton extends Component {
     render() {
         return (
-            <div className="nav-button" style={buttonStyle}>
-                <Link to={this.props.to}>{ this.props.children }</Link>
-            </div>
-        )
+            <Link className="nav-button" style={buttonStyle} to={this.props.to}>
+                {this.props.children}
+            </Link>
+        );
     }
 }
 
 const buttonStyle = {
-    padding: "14px",
-    margin: "14px",
-    float: "right",
-}
+  padding: "14px",
+  margin: "14px",
+  float: "right"
+};
 
-export default NavButton
+export default NavButton;
