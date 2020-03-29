0895ab04b4419e30ab3054218e44daf218590c78
rajeev <rajeev.goonie98@gmail.com>
2020-03-29 13:37:43 -0700

    put regular text for links

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Navigation extends Component {
+    render() {
+        return (
+            <div className="navigation-bar">
+                <p>Rajeev Goonie</p>
+                <p>About</p>
+                <p>Blogs</p>
+                <p>Projects</p>
+                <p>Contact</p>
+                <hr/>
+            </div>
+        )
+    }
+}
+
+export default Navigation
