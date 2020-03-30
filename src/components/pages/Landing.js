18af19dcd3ee67d322475befce872100008a1e32
rajeev <rajeev.goonie98@gmail.com>
2020-03-29 19:27:34 -0700

    centered text content to page

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -3,11 +3,26 @@ import React, { Component } from 'react'
 export class Landing extends Component {
     render() {
         return (
-            <div>
-                <p>Landing Page</p>
+            <div className="page landing" style={pageStyle}>
+                <div className="main-content" style={contentStyle}>
+                    <h1>Rajeev Goonie</h1>
+                    <p>The Programmer with a Life</p>
+                </div>
             </div>
         )
     }
+}
+
+const pageStyle = {
+    //display: "flex",
+}
+
+const contentStyle ={
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
 }
 
 export default Landing
