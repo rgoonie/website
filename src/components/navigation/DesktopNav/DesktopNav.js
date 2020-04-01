aff2f488a412369a6ff16046c9d4632a41e655f1
rajeev <rajeev.goonie98@gmail.com>
2020-03-31 20:38:42 -0700

    restarting navbar

--- src/components/navigation/DesktopNav/DesktopNav.js
+++ src/components/navigation/DesktopNav/DesktopNav.js
@@ -6,20 +6,7 @@ import './DesktopNav.css';
 export class DesktopNav extends Component {
     render() {
         return (
-            <header className="navigation-bar">
-                <div className="content-link">
-                    <Link className="nav-button home" hidden={false} to={ROUTE.LANDING}>
-                        Rajeev Goonie
-                    </Link>
-                </div>
-                
-                
-                <div className="content-links">
-                    <Link className="nav-button about" to={ROUTE.ABOUT}>About</Link>
-                    <Link className="nav-button blogs" to={ROUTE.BLOGS}>Blogs</Link>
-                    <Link className="nav-button projects" to={ROUTE.PROJECTS}>Projects</Link>
-                    <Link className="nav-button contact" to={ROUTE.CONTACT}>Contact</Link>
-                </div>
+            <header>
 
             </header>
         )
