0544cb73dcccda22543103fc56028ed8e5d9727e
rajeev <rajeev.goonie98@gmail.com>
2020-04-02 01:00:34 -0700

    added placeholder text and formatted

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -4,8 +4,19 @@ import './About.css';
 export class About extends Component {
     render() {
         return (
-            <div>
-                <p>About Page</p>
+            <div className="about-page">
+
+                <div className="about-content">
+                    <h1 className="about-header">Who am I?</h1>
+
+                    <div className="about-description">
+                        <p>Lorizzle ipsum mofo sit amizzle, mah nizzle adipiscing elit. Nullam crunk velit, shizznit volutpizzle, mah nizzle hizzle, shizzle my nizzle crocodizzle vel, arcu. Pellentesque eget tortor. Sed erizzle. Ma nizzle izzle dolizzle dapibus turpis tempizzle crackalackin. Mauris pellentesque nibh izzle turpizzle. Check it out in mah nizzle. Bow wow wow eleifend rhoncizzle da bomb. Crackalackin sheezy habitasse gizzle dictumst. Donec dapibizzle. Curabitizzle , pretizzle brizzle, mattizzle ac, eleifend vitae, go to hizzle. I'm in the shizzle suscipit. Integizzle sempizzle velizzle sizzle sheezy.</p>
+                        <p>Lorizzle ipsum mofo sit amizzle, mah nizzle adipiscing elit. Nullam crunk velit, shizznit volutpizzle, mah nizzle hizzle, shizzle my nizzle crocodizzle vel, arcu. Pellentesque eget tortor. Sed erizzle. Ma nizzle izzle dolizzle dapibus turpis tempizzle crackalackin. Mauris pellentesque nibh izzle turpizzle. Check it out in mah nizzle. Bow wow wow eleifend rhoncizzle da bomb. Crackalackin sheezy habitasse gizzle dictumst. Donec dapibizzle. Curabitizzle , pretizzle brizzle, mattizzle ac, eleifend vitae, go to hizzle. I'm in the shizzle suscipit. Integizzle sempizzle velizzle sizzle sheezy.</p>
+                        <p>Lorizzle ipsum mofo sit amizzle, mah nizzle adipiscing elit. Nullam crunk velit, shizznit volutpizzle, mah nizzle hizzle, shizzle my nizzle crocodizzle vel, arcu. Pellentesque eget tortor. Sed erizzle. Ma nizzle izzle dolizzle dapibus turpis tempizzle crackalackin. Mauris pellentesque nibh izzle turpizzle. Check it out in mah nizzle. Bow wow wow eleifend rhoncizzle da bomb. Crackalackin sheezy habitasse gizzle dictumst. Donec dapibizzle. Curabitizzle , pretizzle brizzle, mattizzle ac, eleifend vitae, go to hizzle. I'm in the shizzle suscipit. Integizzle sempizzle velizzle sizzle sheezy.</p>
+                    </div>
+
+                </div>
+
             </div>
         )
     }
