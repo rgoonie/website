f02dd9026cc4415b8f8b6d75cf8d64cdce202383
rajeev <rajeev.goonie98@gmail.com>
2020-04-01 17:02:18 -0700

    added links to buttons

--- src/components/navigation/MoblieNav/MobileNav.js
+++ src/components/navigation/MoblieNav/MobileNav.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import {Drawer, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import {Menu, HomeTwoTone, InfoTwoTone, BookTwoTone, AccountTreeTwoTone, AccountBoxTwoTone} from '@material-ui/icons';
-
+import * as ROUTE from '../routes';
 
 export class MobileNav extends Component{
     constructor(props){
@@ -25,26 +26,40 @@ export class MobileNav extends Component{
                 <Menu onClick={toggleDrawer()}/>
                 <Drawer open={this.state.drawOpen} onClose={toggleDrawer()}>
                     <List>
-                        <ListItem button onClick={toggleDrawer()}>
-                            <ListItemIcon><HomeTwoTone/></ListItemIcon>
-                            <ListItemText primary={"Home"}/>
-                        </ListItem>
-                        <ListItem button onClick={toggleDrawer()}>
-                            <ListItemIcon><InfoTwoTone/></ListItemIcon>
-                            <ListItemText primary={"About"}/>
-                        </ListItem>
-                        <ListItem button onClick={toggleDrawer()} >
-                            <ListItemIcon><BookTwoTone/></ListItemIcon>
-                            <ListItemText primary={"Blogs"}/>
-                        </ListItem>
-                        <ListItem button onClick={toggleDrawer()}>
-                            <ListItemIcon><AccountTreeTwoTone/></ListItemIcon>
-                            <ListItemText primary={"Projects"}/>
-                        </ListItem>
-                        <ListItem button onClick={toggleDrawer()}>
-                            <ListItemIcon><AccountBoxTwoTone/></ListItemIcon>
-                            <ListItemText primary={"Contact"}/>
-                        </ListItem>
+                        <Link to={ROUTE.LANDING}>
+                            <ListItem button onClick={toggleDrawer()}>
+                                <ListItemIcon><HomeTwoTone/></ListItemIcon>
+                                <ListItemText primary={"Home"}/>
+                            </ListItem>
+                        </Link>
+
+                        <Link to={ROUTE.ABOUT}>
+                            <ListItem button onClick={toggleDrawer()}>
+                                <ListItemIcon><InfoTwoTone/></ListItemIcon>
+                                <ListItemText primary={"About"}/>
+                            </ListItem>
+                        </Link>
+
+                        <Link to={ROUTE.BLOGS}>
+                            <ListItem button onClick={toggleDrawer()} >
+                                <ListItemIcon><BookTwoTone/></ListItemIcon>
+                                <ListItemText primary={"Blogs"}/>
+                            </ListItem>
+                        </Link>
+
+                        <Link to={ROUTE.PROJECTS}>
+                            <ListItem button onClick={toggleDrawer()}>
+                                <ListItemIcon><AccountTreeTwoTone/></ListItemIcon>
+                                <ListItemText primary={"Projects"}/>
+                            </ListItem>
+                        </Link>
+
+                        <Link to={ROUTE.CONTACT}>
+                            <ListItem button onClick={toggleDrawer()}>
+                                <ListItemIcon><AccountBoxTwoTone/></ListItemIcon>
+                                <ListItemText primary={"Contact"}/>
+                            </ListItem>
+                        </Link>
                     </List>
                 </Drawer>
             </div>
