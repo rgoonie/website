fc3114969eacd1e96b0f887d5c185e0ff18d4702
rajeev <rajeev.goonie98@gmail.com>
2020-04-01 17:25:18 -0700

    transfered styles to css file

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -5,45 +5,22 @@ import './Landing.css';
 export class Landing extends Component {
     render() {
         return (
-            <div className="page landing" style={pageStyle}>
-                <div className="main-content" style={contentStyle}>
+            <div className="page landing">
+                <div className="main-content">
                     <Typist cursor={cursorStyle}>
-                        <h1 style={headerOneStyle}>Rajeev Goonie</h1>
+                        <h1 className="header">Rajeev Goonie</h1>
                     </Typist>
                     <Typist startDelay={1500} cursor={cursorStyle}>
-                        <span style={paragraphStyle}>The Programmer without a Life</span>
-                        <Typist.Backspace count={10} delay={1000}/>
-                        <span style={paragraphStyle}>{' '} an "Actual" Life</span>
+                        <div className="tag-line">
+                            <span>The Programmer without a Life</span>
+                            <Typist.Backspace count={10} delay={1000}/>
+                            <span>{' '} an "Actual" Life</span>
+                        </div>
                     </Typist>
                 </div>
             </div>
         )
     }
-}
-
-const pageStyle = {
-    //display: "flex",
-}
-
-const contentStyle ={
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-}
-
-const headerOneStyle = {
-    fontSize: "10vh",
-    fontStyle: "normal",
-    fontWeight: "normal",
-}
-
-const paragraphStyle = {
-    fontSize: "3vh",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    display: "inline",    
 }
 
 const cursorStyle = {
