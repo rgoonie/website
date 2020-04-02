a1e81441ae9fa35a5605d886565d6994f0a349e9
rajeev <rajeev.goonie98@gmail.com>
2020-04-02 00:35:27 -0700

    made links into buttons

--- src/components/navigation/Navigation/Naviagation.js
+++ src/components/navigation/Navigation/Naviagation.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import {styled} from '@material-ui/core';
 import * as ROUTE from '../routes';
 import MobileNav from '../MoblieNav/MobileNav'
 
 import './Navigation.css';
+
+const StyledButton = styled(Button)({
+    textTransform: "none",
+    textDecoration: "none",
+    borderRadius: "10px",
+    margin: "0",
+    padding: "0",
+});
 
 export class Navigation extends Component {
     render() {
@@ -13,14 +23,14 @@ export class Navigation extends Component {
                     <div className="mobile-nav">
                         <MobileNav/>
                     </div>
-                    <div className="nav-logo"><Link to={ROUTE.LANDING}>Rajeev Goonie</Link></div>
+                    <div className="nav-logo"><Link to={ROUTE.LANDING}><StyledButton>Rajeev Goonie</StyledButton></Link></div>
                     <div className="spacer"/>
                     <div className="nav-items">
                         <ul>
-                            <li><Link to={ROUTE.ABOUT}> About    </Link></li>
-                            <li><Link to={ROUTE.BLOGS}> Blogs    </Link></li>
-                            <li><Link to={ROUTE.PROJECTS}> Projects </Link></li>
-                            <li><Link to={ROUTE.CONTACT}> Contact  </Link></li>
+                            <li><Link to={ROUTE.ABOUT}>    <StyledButton> About    </StyledButton></Link></li>
+                            <li><Link to={ROUTE.BLOGS}>    <StyledButton> Blog     </StyledButton></Link></li>
+                            <li><Link to={ROUTE.PROJECTS}> <StyledButton> Projects </StyledButton></Link></li>
+                            <li><Link to={ROUTE.CONTACT}>  <StyledButton> Contact  </StyledButton></Link></li>
 
                         </ul>
                     </div>
