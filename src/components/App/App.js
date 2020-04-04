bf8b7dbcae5ad8fc9dd2dcd10d26778d57136d42
rajeev <rajeev.goonie98@gmail.com>
2020-04-04 11:01:01 -0700

    added transition between pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Nav from '../navigation/Navigation/Naviagation';
-import * as ROUTES from '../navigation/routes';
-
-import Landing from '../pages/Landing/Landing';
-import About from '../pages/About/About';
-import Blogs from '../pages/Blogs/Blogs';
-import Projects from '../pages/Projects/Projects';
-import Contact from '../pages/Contact/Contact';
+import PageContainer from '../pages/PageContainer/PageContainer';
 
 import './App.css';
 
@@ -18,12 +12,7 @@ function App() {
       <div className="App">
 
         <Nav/>
-
-        <Route exact path={ROUTES.LANDING} component={Landing}/>
-        <Route exact path={ROUTES.ABOUT} component={About}/>
-        <Route exact path={ROUTES.BLOGS} component={Blogs}/>
-        <Route exact path={ROUTES.PROJECTS} component={Projects} />
-        <Route exact path={ROUTES.CONTACT} component={Contact} />
+        <PageContainer/>
 
       </div>
     </Router>
