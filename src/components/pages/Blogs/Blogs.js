9753309ea5934ebb06402e57b60c5ae4f1ef137f
rajeev <rajeev.goonie98@gmail.com>
2020-04-04 13:45:38 -0700

    added some blogs and styled how they should look on page

--- src/components/pages/Blogs/Blogs.js
+++ src/components/pages/Blogs/Blogs.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import BlogPost from './BlogPost/BlogPost';
+
 import './Blogs.css';
 
 export class Blogs extends Component {
@@ -7,6 +9,19 @@ export class Blogs extends Component {
             <div className="blogs-page">
                 <div className="blogs-content">
                     <h1 className="blogs-header">Blogs Page</h1>
+
+                    <div className="blog-post-all">
+                        <BlogPost>This is a title</BlogPost>
+                        <BlogPost>Short Title</BlogPost>
+                        <BlogPost>Short Title</BlogPost>
+                        <BlogPost content="This is an extension. ">This is a very very long title</BlogPost>
+                        <BlogPost content="This is an extension. ">Title 5</BlogPost>
+                        <BlogPost>Title 6</BlogPost>
+                        <BlogPost>Title 7</BlogPost>
+                        <BlogPost>Title 8</BlogPost>
+                        <BlogPost>Title 9</BlogPost>
+                    </div>
+
                 </div>
             </div>
         )
